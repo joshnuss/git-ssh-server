c5c93d6f51ce75e21e9bdd245599ae48a05945bc
Josh Nussbaum <joshnuss@gmail.com>
2020-05-21 09:39:52 -0400

    Simplify listen()

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ import server from './src/server.js'
 const port = process.env.PORT || 22755
 const address = process.env.ADDRESS || 'localhost'
 
-server.listen(port, address, function() {
-  const {address, port} = this.address()
-
+server.listen(port, address, () => {
   console.log(`server:listen ${address}:${port}`)
 })
