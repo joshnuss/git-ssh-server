ee2b6461567a71eedf8f616624fb2d4b0867bad1
Josh Nussbaum <joshnuss@gmail.com>
2020-05-21 00:25:30 -0400

    Adds key authentication

--- src/authenticate.js
+++ src/authenticate.js
@@ -1,29 +1,40 @@
 import fs from 'fs'
 import glob from 'glob'
 import crypto from 'crypto'
+import ssh2 from 'ssh2'
+
+const { utils } = ssh2
 
 const allowedUser = Buffer.from('git')
 const clientKeys = glob
   .sync('keys/*.pub')
   .map(path => fs.readFileSync(path))
+  .map(data => {
+    const publicKey = utils.parseKey(data)
+    const publicSSHKey = publicKey.getPublicSSH()
+
+    return { publicKey, publicSSHKey }
+  })
 
 export default function authenticate(ctx) {
   const user = Buffer.from(ctx.username)
 
   if (user.length == allowedUser.length && crypto.timingSafeEqual(user, allowedUser) && ctx.method == 'publickey') {
-    const { key } = ctx
-
-    if (isValidKey(key)) {
+    if (isValidKey(ctx)) {
+      console.log('auth:success')
       return ctx.accept()
     }
   }
 
+  console.log('auth:fail')
   ctx.reject()
 }
 
-function isValidKey(key) {
-  // TODO: Security!!!!!! Compare keys
-
-  return true
+function isValidKey({key, blob, signature}) {
+  return !!clientKeys.find(({ publicKey, publicSSHKey }) =>
+    key.algo == publicKey.type
+    && key.data.length == publicSSHKey.length
+    && crypto.timingSafeEqual(key.data, publicSSHKey)
+    && (!signature || publicKey.verify(blob, signature))
+  )
 }
-
